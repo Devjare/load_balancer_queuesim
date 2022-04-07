d88b5016c1f1c38d2acea59209180c02b5c2824f
Andres Reyna <andresreyna15@gmail.com>
2022-04-07 10:18:18 -0500

    Report per worker is written now.

--- execution-instructions.sh
+++ execution-instructions.sh
@@ -2,11 +2,13 @@
 # docker volume create shared
 # Run compoes with environment variables file.
 export WORKERS=$1
-# echo WORKERS=${WORKERS} >> .env.test
+cat .env.test > .env
+echo WORKERS=${WORKERS} >> .env
       # Detached
-# docker-compose --env-file ./.env.test -env WORKERS=${WORKERS} up -d --build --scale queue_sim=$WORKERS
+docker-compose --env-file ./.env.test up -d --build --scale queue_sim=$WORKERS
       # Attached
-docker-compose --env-file ./.env.test up --build --scale queue_sim=$WORKERS
+# docker-compose --env-file ./.env up --build --scale queue_sim=$WORKERS
+rm ./.env
 
 echo "\nQUEUE OUTPUT: \n"
-cat /var/lib/docker/volumes/shared/_data/queue_output_worker_*
+cat /var/lib/docker/volumes/shared/_data/T1/queue_output.workers
